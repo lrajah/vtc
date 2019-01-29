4f207890d9c1a20085a90d2977c06caaa8f94b95
Dell <Dell@10.31.70.38>
2019-01-29 09:26:29 +0100

    comit correction mineur

--- src/main/java/com/formation/vtc/controller/ClientController.java
+++ src/main/java/com/formation/vtc/controller/ClientController.java
@@ -3,7 +3,9 @@ package com.formation.vtc.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formation.vtc.dto.ReservationListItem;
@@ -16,7 +18,8 @@ public class ClientController {
 	@Autowired
 	TrajetService trajetServ;
 	
-	
+	@GetMapping
+	@ResponseBody
 	public List<ReservationListItem> findByNumResa(List<String> numRes){
 		return trajetServ.findByResa(numRes);
 	}
