d791b7ba1258dba93e13275bed2d472d22819931
Dell <Dell@10.31.70.38>
2019-01-30 16:37:11 +0100

    Commit heure en db

--- src/main/java/com/formation/vtc/persistence/repository/HeureRepository.java
+++ src/main/java/com/formation/vtc/persistence/repository/HeureRepository.java
@@ -1,5 +1,7 @@
 package com.formation.vtc.persistence.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,4 +10,6 @@ import com.formation.vtc.persistence.entity.Heure;
 public interface HeureRepository extends JpaRepository<Heure,Long>{
 	@Query(value="SELECT * FROM heure WHERE heureDepart=?1", nativeQuery = true)
 	Heure findByHeure(Long heure);
+	@Query(value="SELECT * FROM heure", nativeQuery = true)
+	List<Heure> findAll();
 }
