fcbbac4daa3656d159abab1db92cfa0f8861c806
Dell <Dell@10.31.70.88>
2019-02-01 12:55:20 +0100

    Ajout facture

--- src/main/java/com/formation/vtc/service/IAdminService.java
+++ src/main/java/com/formation/vtc/service/IAdminService.java
@@ -1,7 +1,9 @@
 package com.formation.vtc.service;
 
+import com.formation.vtc.dto.ReservationItem;
 import com.formation.vtc.dto.TrajetListItem;
 
 public interface IAdminService {
 	TrajetListItem cancelTournee(long id);
+	ReservationItem findResa(String resa);
 }
--- src/main/java/com/formation/vtc/service/impl/AdminService.java
+++ src/main/java/com/formation/vtc/service/impl/AdminService.java
@@ -8,9 +8,13 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.formation.vtc.dto.ReservationItem;
+import com.formation.vtc.dto.ReservationListItem;
 import com.formation.vtc.dto.TrajetListItem;
 import com.formation.vtc.exception.InvalidOperationException;
+import com.formation.vtc.persistence.entity.Reservation;
 import com.formation.vtc.persistence.entity.Trajet;
+import com.formation.vtc.persistence.repository.ReservationRepository;
 import com.formation.vtc.persistence.repository.TrajetRepository;
 import com.formation.vtc.service.IAdminService;
 
@@ -20,6 +24,9 @@ public class AdminService implements IAdminService{
 	
 	@Autowired
 	TrajetRepository trajetRepo;
+	
+	@Autowired
+	ReservationRepository reservationRepo;
 	
 	@Override
 	public TrajetListItem cancelTournee(long id) {
@@ -41,6 +48,17 @@ public class AdminService implements IAdminService{
 		} else throw new InvalidOperationException("Le trajet : "+id+" n'a pas été trouvé");
 		
 		return new TrajetListItem(opt.get());
+	}
+	
+	@Override
+	public ReservationItem findResa(String numResa) {
+		Optional<Reservation> opt = reservationRepo.findByNumResaList(numResa);
+		
+		if(opt.isPresent()) {
+			if(opt.get().getEtatResa().equals("Valide")) return new ReservationItem(opt.get());
+			if(!opt.get().getEtatResa().equals("Valide")) throw new InvalidOperationException("Le numéro de réservation : "+numResa+" n'a pas encore été validé");
+		} else throw new InvalidOperationException("Le numéro de réservation : "+numResa+" n'existe pas");
+		return null;
 	}
 	
 }
